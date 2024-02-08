26e47b2d0be9e423cf658fcee2f88f0c58608435
hawa3422 <143721624+hawa3422@users.noreply.github.com>
2024-02-08 13:40:34 +0100

    modified:   index.html modified:   script.js modified:   style.css

--- script.js
+++ script.js
@@ -2,4 +2,34 @@
 
 //hawa
 
+$(document).ready(function() {
+    
+    const HomeButton = $('<button>', {
+        id: 'HomeButton',
+        class: 'Home-button',
+        text: 'Hem'
+    });
+
+    const DiscoverButton = $('<button>', {
+        id: 'DiscoverButton',
+        class: 'Discover-button',
+        text: 'Upptäck'
+    });
+
+    const Savedbutton = $('<button>', {
+        id: 'Saved',
+        class: 'Saved-button',
+        text: 'Sparade'
+    });
+    
+    $('#UI').append(HomeButton, DiscoverButton, Savedbutton);
+
+    
+    $("#searchButton").click(function() {
+        var searchTerm = $("#searchInput").val().trim();
+        
+    
+    });
+});
+
 //oliver
